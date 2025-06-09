d0f50543fe4dd1706cb157560d0657c6c1778aa0
muaazkhaaan <148960629+muaazkhaaan@users.noreply.github.com>
2025-06-09 13:16:50 +0100

    Find the file, print the contents and copy over

--- Task1/task_1.py
+++ Task1/task_1.py
@@ -1,12 +1,13 @@
 import os
+import shutil
 
 # Step 1: Ask for filename
 target_filename = input("Enter the name of the file to search for (e.g. 'Yorkshire Tea.txt'): ")
-input_dir = os.path.join(os.getcwd(), 'Input')  # Script is in Task1
+input_dir = os.path.join(os.getcwd(), 'Input')  # Scripts are in Input - only folder it should search through
 
 found_path = None
 
-# Search through 'Input/' recursively for the target file
+# Search through 'Input/' recursively for the target file or break
 for root, dirs, files in os.walk(input_dir):
     if target_filename in files:
         found_path = os.path.join(root, target_filename)
@@ -14,5 +15,25 @@ for root, dirs, files in os.walk(input_dir):
 
 if found_path:
     print(f"File found at: {found_path}")
+
+    # Step 2: Read and print the contents of the file
+    try:
+        with open(found_path, 'r') as file:
+            content = file.read()
+            print(f"\nContents of '{target_filename}'\n{content}\n")
+    except Exception as e:
+        print(f"Failed to read the file: {e}")
+
+    # Step 3: Copy file to the 'Ouput' folder
+    output_dir = os.path.join(os.getcwd(), 'Ouput')
+
+    destination_path = os.path.join(output_dir, target_filename)
+
+    try:
+        shutil.copy2(found_path, destination_path)
+        print(f"File copied to: {destination_path}")
+    except Exception as e:
+        print(f"Failed to copy file: {e}")
+
 else:
     print("File not found.")
