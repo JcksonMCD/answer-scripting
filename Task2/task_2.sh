acb9fc103d7411015478f15db866a55e560e6176
muaazkhaaan <148960629+muaazkhaaan@users.noreply.github.com>
2025-06-09 14:21:29 +0100

    Take on a variable name and print back out to confirm it is correct

--- Task2/task_2.sh
+++ Task2/task_2.sh
@@ -1,2 +1,13 @@
-#! /usr/bin/bash
-echo "Hello World"
+#!/bin/bash
+
+# Step 1: Ask user for the API URL
+read -p "Enter the API URL: " api_url
+
+# If blank exit script
+if [ -z "$api_url" ]; then
+  echo "No URL provided. Exiting."
+  exit 1
+fi                                          # Closing if statement - bash way of doing if statements
+
+# Print
+echo "You entered: $api_url"
