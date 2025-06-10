6069b6f2941a51ef73097b6d2ced23719692f621
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-10 09:28:32 +0100

    Pseudocode method and param collection from user added to script.

--- Task1/task_1.ps1
+++ Task1/task_1.ps1
@@ -1,1 +1,14 @@
 #requires -PSEdition Core
+
+# Collect user input of file name
+param ($FileName = $(throw "FileName parameter is required."))
+
+# Search for the file of that name (use try catch for if it is not found)
+
+# Copy file contents
+
+# Print contents to console
+
+# Print contents to a new file in the output folder
+
+# Delete old file
