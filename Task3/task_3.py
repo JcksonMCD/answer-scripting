4c40e62e813e1225be98e5a5668fb657720e4c5e
Robb <robbmorgan10@gmail.com>
2025-06-10 09:27:30 +0100

    moved task 3 folders over to the wsl mnt and changed .home to .cwd . Made caculations

--- Task3/task_3.py
+++ Task3/task_3.py
@@ -1,13 +1,37 @@
 from pathlib import Path 
+import json
 
-file_path = Path.home()
+filePath = Path.cwd()
+print(filePath)
+
+rootDir = filePath.anchor
+rootPath = Path(rootDir)
+
 targetFile = input('Enter file name: ')
-textFiles = list(file_path.rglob(targetFile))
+textFiles = list(rootPath.rglob(targetFile))
 
 if textFiles:
     file_to_open = textFiles[0]
     with file_to_open.open('r', encoding='utf-8') as f:
         content = f.read()
-    print(content)
+
+    data = json.loads(content)
+
+    add = sum(data.get("Add", []))
+    minus = data.get("Minus", [])
+    minusTotal = minus[0]
+    
+    for num in minus[1:]:
+            minusTotal -= num
+
+    multiply = 1
+    for num in data.get("Times", []):
+            multiply *= num
+
+    print(f"Addition Results: {add}")
+    print(f"Subtraction Results: {minusTotal}")
+    print(f"Results of times: {multiply}")
+
 else:
     print("File not found.")
+
