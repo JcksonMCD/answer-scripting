fd13b690c30e5e9a3917f9c2a8ba9171ab917be9
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-10 11:38:15 +0100

    Error handling added for if file does not exist.

--- Task1/task_1.ps1
+++ Task1/task_1.ps1
@@ -4,11 +4,23 @@
 param ($FileName = $(throw "FileName parameter is required."))
 
 # Search for the file of that name (use try catch for if it is not found)
+try {
+    $File=Get-ChildItem $FileName -Recurse -File -ErrorAction SilentlyContinue
+}
+catch {
+    Console.WriteLine($"This file does not exist");
+}
+
+Set-Location $File.DirectoryName
 
 # Copy file contents
+$FileText = Get-Content $FileName
 
 # Print contents to console
+Write-Output $FileText
 
 # Print contents to a new file in the output folder
+Out-File -Force -Path "C:\Users\JacksonMacDonald\Documents\Answer Academy\Scripting\answer-scripting\Task1\Output\$FileName" -Encoding UTF8 -InputObject $FileText
 
 # Delete old file
+Remove-Item $File
